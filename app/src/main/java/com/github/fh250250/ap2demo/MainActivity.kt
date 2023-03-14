e8055749d8812dcfdb1265f1e36409daf20ba049
Gasper <duanpengyuan@qq.com>
2023-03-14 12:55:21 +0800

    update

--- app/src/main/java/com/github/fh250250/ap2demo/MainActivity.kt
+++ app/src/main/java/com/github/fh250250/ap2demo/MainActivity.kt
@@ -1,18 +1,62 @@
 package com.github.fh250250.ap2demo
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.SurfaceView
 import androidx.appcompat.app.AppCompatActivity
+import com.github.fh250250.ap2.lib.AudioStreamInfo
+import com.github.fh250250.ap2.lib.VideoStreamInfo
+import com.github.fh250250.ap2.server.AirPlayConfig
+import com.github.fh250250.ap2.server.AirPlayConsumer
+import com.github.fh250250.ap2.server.AirPlayServer
+import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
-    lateinit var surfaceView: SurfaceView
-    lateinit var server: Server
+class MainActivity : AppCompatActivity(), AirPlayConsumer {
+    private val TAG = "ap2demo"
+
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var airPlayServer: AirPlayServer
+    private lateinit var videoPlayer: VideoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         surfaceView = findViewById(R.id.surfaceView)
-        server = Server(surfaceView)
+
+        val airPlayConfig = AirPlayConfig()
+        airPlayConfig.serverName = Build.MODEL
+        airPlayConfig.width = 1920
+        airPlayConfig.height = 1080
+        airPlayConfig.fps = 25
+        airPlayServer = AirPlayServer(airPlayConfig, this)
+        thread { airPlayServer.start() }
+
+        videoPlayer = VideoPlayer(surfaceView)
+    }
+
+    override fun onVideoFormat(videoStreamInfo: VideoStreamInfo?) {
+        Log.i(TAG, "onVideoFormat")
+    }
+
+    override fun onVideo(bytes: ByteArray?) {
+        bytes?.let { videoPlayer.addPacket(it) }
+    }
+
+    override fun onVideoSrcDisconnect() {
+        videoPlayer.stop()
+    }
+
+    override fun onAudioFormat(audioStreamInfo: AudioStreamInfo?) {
+        Log.i(TAG, "onAudioFormat")
+    }
+
+    override fun onAudio(bytes: ByteArray?) {
+        Log.i(TAG, "onAudio")
+    }
+
+    override fun onAudioSrcDisconnect() {
+        Log.i(TAG, "onAudioSrcDisconnect")
     }
 }
--- app/src/main/java/com/github/fh250250/ap2demo/VideoPlayer.kt
+++ app/src/main/java/com/github/fh250250/ap2demo/VideoPlayer.kt
@@ -3,83 +3,74 @@ package com.github.fh250250.ap2demo
 import android.media.MediaCodec
 import android.media.MediaFormat
 import android.util.Log
+import android.view.SurfaceHolder
 import android.view.SurfaceView
-import java.nio.ByteBuffer
-import java.util.*
-import kotlin.concurrent.thread
+import java.util.concurrent.LinkedBlockingQueue
 
-class VideoPlayer(val surfaceView: SurfaceView) {
+class VideoPlayer(private val surfaceView: SurfaceView) : SurfaceHolder.Callback {
     private val TAG = "VideoPlayer"
 
-    private val codec: MediaCodec
-    private val packetList = Collections.synchronizedList(ArrayList<ByteArray>())
-    private var isEnd = false
+    private val packetList = LinkedBlockingQueue<ByteArray>()
+    private lateinit var codec: MediaCodec
 
     init {
-        val mediaFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, surfaceView.width, surfaceView.height)
-        codec = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
-        codec.setVideoScalingMode(MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING)
-        codec.configure(mediaFormat, surfaceView.holder.surface, null, 0)
-        codec.start()
+        surfaceView.holder.addCallback(this)
+    }
 
-        thread { mainLoop() }
+    fun stop() {
+        packetList.clear()
     }
 
     fun addPacket(packet: ByteArray) {
         packetList.add(packet)
     }
 
-    private fun mainLoop() {
-        while (!isEnd) {
-            if (packetList.size == 0) {
-                Thread.sleep(50)
-                continue
+    private fun setupCodec() {
+        val mediaFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, surfaceView.width, surfaceView.height)
+        codec = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
+        codec.setVideoScalingMode(MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT)
+        codec.setCallback(object : MediaCodec.Callback() {
+            override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {
+                val packet = packetList.take()
+                val buffer = codec.getInputBuffer(index)
+
+                buffer?.put(packet)
+                codec.queueInputBuffer(index, 0, packet.size, 0, 0)
             }
-            decode(packetList.removeAt(0))
-        }
+
+            override fun onOutputBufferAvailable(
+                codec: MediaCodec,
+                index: Int,
+                info: MediaCodec.BufferInfo
+            ) {
+                codec.releaseOutputBuffer(index, true)
+            }
+
+            override fun onError(codec: MediaCodec, e: MediaCodec.CodecException) {
+                Log.e(TAG, "onError", e)
+            }
+
+            override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
+                Log.i(TAG, "onOutputFormatChanged")
+            }
+
+        })
+        codec.configure(mediaFormat, surfaceView.holder.surface, null, 0)
+        codec.start()
     }
 
-    private fun decode(packet: ByteArray) {
-        val TIMEOUT_USEC = 10_000L
-        val decoderInputBuffers: Array<ByteBuffer> = codec.getInputBuffers()
-        var inputBufIndex = -10000
-        try {
-            inputBufIndex = codec.dequeueInputBuffer(TIMEOUT_USEC)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        if (inputBufIndex >= 0) {
-            val inputBuf: ByteBuffer = decoderInputBuffers[inputBufIndex]
-            inputBuf.put(packet)
-            codec.queueInputBuffer(inputBufIndex, 0, packet.size, 0, 0)
-        } else {
-            Log.d(TAG, "dequeueInputBuffer failed")
-        }
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        setupCodec()
+    }
 
-        var outputBufferIndex = -10000
-        try {
-            val mBufferInfo = MediaCodec.BufferInfo()
-            outputBufferIndex = codec.dequeueOutputBuffer(mBufferInfo, TIMEOUT_USEC)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        if (outputBufferIndex >= 0) {
-            codec.releaseOutputBuffer(outputBufferIndex, true)
-            try {
-                Thread.sleep(50)
-            } catch (ie: InterruptedException) {
-                ie.printStackTrace()
-            }
-        } else if (outputBufferIndex == MediaCodec.INFO_TRY_AGAIN_LATER) {
-            try {
-                Thread.sleep(10)
-            } catch (ie: InterruptedException) {
-                ie.printStackTrace()
-            }
-        } else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
-            // not important for us, since we're using Surface
-        } else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
-        } else {
-        }
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        codec.stop()
+        codec.release()
+        setupCodec()
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        codec.stop()
+        codec.release()
     }
 }
